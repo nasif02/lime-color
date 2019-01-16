65422316e5ad687623d1f0eec086812a5ad5c938
Xplo <nasif.002@gmail.com>
2019-01-16 12:15:15 +0600

    add color utils class

---
+++ matcolor/src/main/java/matcolor/base/Contextor.java
@@ -0,0 +1,36 @@
+package matcolor.base;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+/**
+ * Copyright 2018 (C) Xplo
+ * <p>
+ * Created  : 10/23/2018
+ * Author   : Xplo
+ * Version  : 1.0
+ * Desc     : alime.utils
+ * Comment  :       Contextor.getInstance().init(appContext);
+ */
+public class Contextor {
+
+    @SuppressLint("StaticFieldLeak")
+    private static Contextor sInstance;
+
+    private Context mContext;
+
+    public static Contextor getInstance() {
+        if (sInstance == null) {
+            sInstance = new Contextor();
+        }
+        return sInstance;
+    }
+
+    public void init(Context context) {
+        mContext = context;
+    }
+
+    public Context getContext() {
+        return mContext;
+    }
+}
---
+++ matcolor/src/main/java/matcolor/utils/ColorUtils.java
@@ -0,0 +1,71 @@
+package matcolor.utils;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import matcolor.base.Contextor;
+
+/**
+ * Copyright 2019 (C) Xplo
+ * <p>
+ * Created  : 1/16/2019
+ * Author   : Xplo
+ * Version  : 1.0
+ * Desc     : matcolor.utils
+ * Comment  :
+ */
+public class ColorUtils {
+
+    private static final Context context = Contextor.getInstance().getContext();
+
+
+    private int getMaterialColor(int resIdColor) {
+        int returnColor = context.getResources().getColor(resIdColor);
+        return returnColor;
+    }
+
+    private int getMaterialColorRandom(int resIdColorArray) {
+        int returnColor = Color.GRAY;
+        TypedArray colors = context.getResources().obtainTypedArray(resIdColorArray);
+        int index = (int) (Math.random() * colors.length());
+        returnColor = colors.getColor(index, Color.GRAY);
+        colors.recycle();
+        return returnColor;
+    }
+
+
+    private List<Integer> getMaterialColorList(int resIdColorArray) {
+        TypedArray colors = context.getResources().obtainTypedArray(resIdColorArray);
+        List<Integer> items = new ArrayList<>();
+        for (int i = 0; i < colors.length(); i++) {
+            items.add(colors.getColor(i, Color.GRAY));
+        }
+        colors.recycle();
+
+        return items;
+    }
+
+
+    /**
+     * Chooses random color defined in res/array.xml
+     */
+    private int getRandomMaterialColor() {
+        int returnColor = Color.GRAY;
+        //int arrayId = context.getResources().getIdentifier("mdcolor_" + typeColor, "array", context.getPackageName());
+        int arrayId = context.getResources().getIdentifier("mdcolor_400", "array", context.getPackageName());
+
+        if (arrayId != 0) {
+            TypedArray colors = context.getResources().obtainTypedArray(arrayId);
+            int index = (int) (Math.random() * colors.length());
+            returnColor = colors.getColor(index, Color.GRAY);
+            colors.recycle();
+        }
+        return returnColor;
+    }
+
+
+}
